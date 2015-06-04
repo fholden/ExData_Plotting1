5ac96ee8264c84960633db82220713fe50b12ab9
Frank Holden <fholden@me.com>
2015-06-04 09:43:45 -0400

    fixed lableing
    put a mast title on #4

--- Plot4.R
+++ Plot4.R
@@ -28,18 +28,19 @@ epcFull$DateTime <- strptime(epcFull$DateTime,format="%d/%m/%Y %H:%M:%S")
 
 # do Plot 4
 png(filename="Plot4.png",width = 480, height = 480)
-old.par <- par(mfrow=c(2,2))
+old.par <- par(mfrow=c(2,2),mar=c(4,4,2,1), oma=c(0,0,2,0))
 #1
-plot(epcFull$DateTime, epcFull$Global_active_power, type="l",main="Power Consumption Feb 1 and 2, 2007",xlab="Day of the Week",ylab="Global Active Power (kilowatts)")
+plot(epcFull$DateTime, epcFull$Global_active_power, type="l",xlab="Day of the Week",ylab="Global Active Power (kilowatts)")
 #2
-plot(epcFull$DateTime, epcFull$Voltage, type="l",main="Power Consumption Feb 1 and 2, 2007",xlab="Day of the Week",ylab="Voltage")
+plot(epcFull$DateTime, epcFull$Voltage, type="l",xlab="Day of the Week",ylab="Voltage")
 #3
-plot(epcFull$DateTime, epcFull$Sub_metering_1, type="l",main="Power Consumption Feb 1 and 2, 2007",xlab="Day of the Week",ylab="Energy sub metering (kilowatts)")
+plot(epcFull$DateTime, epcFull$Sub_metering_1, type="l",xlab="Day of the Week",ylab="Energy sub metering (kilowatts)")
 lines(epcFull$DateTime,epcFull$Sub_metering_2,col="red")
 lines(epcFull$DateTime,epcFull$Sub_metering_3,col="blue")
 legend("topright", 1:3, cex=0.8,legend=c("sub meter 1","sub meter 2","sub meter 3"), pch=-1,lty=1,col=c("black","red","blue"))
 #4
-plot(epcFull$DateTime, epcFull$Global_reactive_power, type="l",main="Power Consumption Feb 1 and 2, 2007",xlab="Day of the Week",ylab="Global reactive Power (kilowatts)")
+plot(epcFull$DateTime, epcFull$Global_reactive_power, type="l",xlab="Day of the Week",ylab="Global reactive Power (kilowatts)")
+title("Power Consumption Feb 1 and 2, 2007",outer=T)
 par(old.par)
 dev.off()
 
